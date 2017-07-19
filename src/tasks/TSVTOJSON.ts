6ce7e2fde69701ea28a11c3e5a72b7aa26f04590
deegles <diego@vokkal.co>
2017-07-18 19:40:32 -0700

    Add TSV to JSON conversion task.

--- src/tasks/TSVTOJSON.ts
+++ src/tasks/TSVTOJSON.ts
@@ -1,13 +1,31 @@
-import {TaskResponse} from "../index";
+import {TaskDefinition, TaskResponse} from "../index";
+import * as fs from "fs";
 
-export default function (files: Array<string>, params: any): TaskResponse {
-    console.log("destination: " + params.path);
+export default function (files: Array<string>, params: TaskDefinition): Promise<TaskResponse> {
 
-    files.forEach(file => {
-        console.log("file: " + file);
+    return new Promise((resolve, reject) => {
+        let results: Array<string> = [];
+        try {
+            files.forEach(file => {
+                let raw = fs.readFileSync(file).toString("utf8");
+                let dest = file.slice(0, file.length - 4) + ".json";
+
+                raw.split("\n").forEach(line => {
+                    if (line.length > 0) {
+                        fs.appendFileSync(dest, JSON.stringify(line.split("\t")) + "\n");
+                    }
+                });
+
+                results.push(dest);
+            });
+
+            resolve({
+                message: `Processed file count: ${results.length}`,
+                files: results
+            });
+
+        } catch (err) {
+            reject(err);
+        }
     });
-    return {
-        message: `Loaded file: ${params["url"]}`,
-        files: ["fakefile"]
-    };
 }
