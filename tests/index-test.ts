a2ad541baa4495a73c214a8f127cc5515a4be9ef
deegles <diego@vokkal.co>
2017-07-18 17:55:17 -0700

    Add test for LOADTSV.

--- tests/index-test.ts
+++ tests/index-test.ts
@@ -1,7 +1,33 @@
 import {expect} from "chai";
+import * as os from "os";
+import {TaskDefinition, TaskResponse} from "../src/index";
+import {LoadTSVTaskDefinition} from "../src/tasks/LOADTSV";
+import * as fs from "fs";
 
-describe("obj", () => {
-    it("can use context", () => {
-       expect("a").equal("a");
+let tasksPath = "../src/tasks";
+let outputPathRoot = `${os.tmpdir()}/RUN_${Date.now()}/`;
+
+// Need to create destination directory before running
+fs.mkdirSync(outputPathRoot);
+
+describe("LOADTSV", () => {
+    it("can fetch TSV from a URL", (done) => {
+
+        let taskDef: LoadTSVTaskDefinition = {
+            name: "LOADTSV",
+            desc: "LOADTSV test",
+            path: outputPathRoot,
+            url: "https://raw.githubusercontent.com/enigma-io/workflow-interview-challenge/master/inventory.tsv"
+        };
+
+        let task = require(__dirname + "/" + tasksPath + "/" + taskDef.name).default as (files: Array<string>, taskDef: LoadTSVTaskDefinition) => Promise<TaskResponse>;
+
+        task([], taskDef).then(result => {
+            expect(result.files[0]).equal(outputPathRoot + "inventory.tsv");
+            done();
+        }).catch(err => {
+            console.log(err);
+            expect(err).to.be.empty;
+        });
     });
 });
