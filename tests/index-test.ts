efc817b77aa768f7e6340f6624a46f9556dce05c
deegles <diego@vokkal.co>
2017-07-19 08:49:35 -0700

    Add test for TSVTOJSON

--- tests/index-test.ts
+++ tests/index-test.ts
@@ -3,9 +3,10 @@ import * as os from "os";
 import {TaskDefinition, TaskResponse} from "../src/index";
 import {LoadTSVTaskDefinition} from "../src/tasks/LOADTSV";
 import * as fs from "fs";
+import * as assert from "assert";
 
 let tasksPath = "../src/tasks";
-let outputPathRoot = `${os.tmpdir()}/RUN_${Date.now()}/`;
+let outputPathRoot = `${os.tmpdir()}/TEST_${Date.now()}/`;
 
 // Need to create destination directory before running
 fs.mkdirSync(outputPathRoot);
@@ -28,6 +29,37 @@ describe("LOADTSV", () => {
         }).catch(err => {
             console.log(err);
             expect(err).to.be.empty;
+        });
+    });
+});
+
+describe("TSVTOJSON", () => {
+    it("can convert TSV to a JSON", (done) => {
+
+        let taskDef: TaskDefinition = {
+            name: "TSVTOJSON",
+            desc: "TSVTOJSON test",
+            path: outputPathRoot,
+            parallel: false
+        };
+
+        let tsvPath = __dirname + "/tsv/inventory.tsv";
+        let task = require(__dirname + "/" + tasksPath + "/" + taskDef.name).default as (files: Array<string>, taskDef: TaskDefinition) => Promise<TaskResponse>;
+
+        task([tsvPath], taskDef).then(result => {
+            let target = fs.readFileSync(outputPathRoot + "inventory.json").toString("utf8");
+            let json = JSON.parse(target);
+
+            expect(json).to.not.be.empty;
+
+            let header = ["Rank", "Port", "Volume 2015", "Volume 2014", "Volume 2013", "Volume 2012", "Volume 2011"];
+
+            assert.deepEqual(header, json["data"][0]);
+            done();
+        }).catch(err => {
+            console.log(err);
+            expect(err).to.be.empty;
+            done();
         });
     });
 });
